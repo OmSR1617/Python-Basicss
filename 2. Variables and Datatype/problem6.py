8c1bbb1a0fa45d91a992ceb14300d45e908c24bf
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2025-03-23 02:38:13 +0530

    1 and 2 module

---
+++ 2. Variables and Datatype/problem6.py
@@ -0,0 +1,3 @@
+a = (int(input("Enter value a: ")))
+
+print("Square of input is", a*a) #can also be written as a**2
---
+++ 2. Variables and Datatype/variables.py
@@ -0,0 +1,7 @@
+a = 1 # a is variable/identifier
+
+b = 4
+
+name = "Sakshad"
+
+print(a+b)
