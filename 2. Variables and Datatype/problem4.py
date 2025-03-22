c20b5756cd44b4eccdd736486a0d1ad1eb1d1392
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2025-03-23 02:57:42 +0530

    module 2 commit

---
+++ 2. Variables and Datatype/problem4.py
@@ -0,0 +1,5 @@
+a = (int(input("Enter value a: ")))
+
+b = (int(input("Enter value b: ")))
+
+print("A is greater than B is", a>b)
---
+++ 2. Variables and Datatype/problem5.py
@@ -0,0 +1,5 @@
+a = (int(input("Enter value a: ")))
+
+b = (int(input("Enter value b: ")))
+
+print("average of a and b is", (a+b)/2)
