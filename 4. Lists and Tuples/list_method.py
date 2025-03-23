994567b68309ca9d7577a0d6e5923db86e793706
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2025-03-24 02:41:22 +0530

    Create list_method.py

---
+++ 4. Lists and Tuples/list_method.py
@@ -0,0 +1,13 @@
+list = ["Apple", "Orange", 5, 345.6, False, "Aakash", "Sakshad"]
+print(list)
+
+list.append("Badmosh")
+print(list)
+
+l1 = [32, 56, 12, 67, 9, 29]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3, 3333)
+l1.pop(3)
+
+print(l1)
