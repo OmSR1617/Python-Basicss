57b587cbe52ed6297946f34f0a53ae6c8f4343c9
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2025-03-24 00:06:21 +0530

    Create problem3.py

---
+++ 3. String/problem3.py
@@ -0,0 +1,5 @@
+name = ("Sakshad is  a good boy")
+print(name.find("  ")) # if a sentence has double face, it will return its index
+
+name1 = ("Sakshad is a good boy")
+print(name1.find("  ")) # if a sentence doesn't has double face, it will return -1
