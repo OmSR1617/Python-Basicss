d3d589003688abbb0479d7f3e605736ad0e3b92a
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2025-03-23 23:47:55 +0530

    Create escape_seq.py

---
+++ 3. String/escape_seq.py
@@ -0,0 +1,9 @@
+a = "Sakshad is a good boy\nbut not a bad boy" # \n creates a new line in output
+b = "Sakshad is a good boy\tbut not a bad boy" # \t creates a tab space in output
+c = "Sakshad is a good boy \"but\" not a bad boy" # \" make word in double quote in output
+d = 'Sakshad is a good boy \'but\' not a bad boy' # \' make word in single quote in output
+
+print(a)
+print(b)
+print(c)
+print(d)
