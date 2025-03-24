3f55553d8d3bba83e88ed3b0c529c5e8de93ba2d
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2025-03-24 22:47:03 +0530

    Create problem6.py

---
+++ 5. Dictionary & Sets/problem6.py
@@ -0,0 +1,17 @@
+d = {}
+
+name = input("Enter your name: ")
+lang = input("Enter language: ")
+d.update({name: lang})
+
+name = input("Enter your name: ")
+lang = input("Enter language: ")
+d.update({name: lang})
+
+name = input("Enter your name: ")
+lang = input("Enter language: ")
+d.update({name: lang})
+
+name = input("Enter your name: ")
+lang = input("Enter language: ")
+d.update({name: lang})
