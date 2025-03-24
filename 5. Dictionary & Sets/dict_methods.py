416009b111343139c044686670770c7ca4cc4422
Sakshad Ramgude <70849480+OmSR1617@users.noreply.github.com>
2025-03-24 21:30:49 +0530

    Create dict_methods.py

---
+++ 5. Dictionary & Sets/dict_methods.py
@@ -0,0 +1,17 @@
+marks = { 
+         "Sakshad": 100,
+         "Harsh" : 92,
+         "Shubham" : 80,
+         0 : "Sakshad"} 
+
+# print(marks.items())
+
+# print(marks.keys())
+
+# print(marks.values())
+
+# marks.update({"Sakshad": 99, "Badmos": 88})
+# print(marks)
+
+print(marks.get("Sakshad2")) #Prints None
+print(marks["Sakshad2"]) #Prints Error
